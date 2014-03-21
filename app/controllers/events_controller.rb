cde03124fccb36f2af0a5fc4c71c8e7b619da241
thomasdane <tjdane@gmail.com>
2014-03-22 01:11:42 +1100

    coding on branch

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -13,6 +13,11 @@ class EventsController < ApplicationController
   end
 
   def show
+     
+
+    url = %w[http://www.sydneytalks.com.au/index2.php?option=com_rss&no_html=1&catid=16]
+    feed = Feedjira::Feed.fetch_and_parse url 
+    @rss_event = feed.values.first.entries
   end
 
   def update
