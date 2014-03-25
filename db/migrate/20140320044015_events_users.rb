3b63ccc4d4cfb8f3783ef37257b5cec6bfea9196
thomasdane <tjdane@gmail.com>
2014-03-25 14:33:19 +1100

    test

--- db/migrate/20140320044015_events_users.rb
+++
@@ -1,8 +0,0 @@
-class EventsUsers < ActiveRecord::Migration
-  def change
-    create_table :events_users, :id => false do |t|
-      t.integer :events_id
-      t.integer :users_id
-    end
-  end
-end
