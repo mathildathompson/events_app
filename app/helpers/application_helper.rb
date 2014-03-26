d5451c2a3bde70536ea66803fce7d832b906d396
thomasdane <tjdane@gmail.com>
2014-03-26 21:22:11 +1100

    changed logout message

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   def intellinav
     links = ''
     if @current_user
-      links += "<li>#{ link_to('Log out ' + @current_user.first_name, login_path, :method => :delete, :confirm => 'Really log out?') }</li>"
+      links += "<li>#{ link_to('Hi ' + @current_user.first_name + '... log out', login_path, :method => :delete) }</li>"
     else
       links += "<li>#{ link_to('Sign in', login_path) }</li>
       <li>#{ link_to('Sign up', new_user_path) }</li>"
